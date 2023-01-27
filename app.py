9d82b483fafce19a55c706f28c4c1478eb63eba6
kozanakyel <kozanakyel@gmail.com>
2023-01-27 16:25:46 +0300

    added openai, telegram and gtts for text to speech

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-
-from flask_marshmallow import Marshmallow
-from marshmallow import fields
-from flask_migrate import Migrate
+import openai
+from telegram import Bot
+import asyncio
+from gtts import gTTS
+from io import BytesIO
 
 load_dotenv()
+
+app = Flask(__name__)
 
 PSQL_USER_ENV=os.getenv('PSQL_USER')
 PSQL_PWD_ENV=os.getenv('PSQL_PWD')
@@ -15,66 +18,38 @@ PSQL_URI_ENV=os.getenv('PSQL_URI')
 PSQL_PORT_ENV=os.getenv('PSQL_PORT')
 PSQL_DB_NAME_ENV=os.getenv('PSQL_DB_NAME')
 
-app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/flask_test'
+CHATGPT_ENV=os.getenv('CHATGPT')
 
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-migrate = Migrate(app, db)
+TELEGRAM_BOT_API_ENV = os.getenv('TELEGRAM_BOT_API')
 
-class Author(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    specialisation = db.Column(db.String(50))
-    
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-    
-    def __init__(self, name, specialisation):
-        self.name = name
-        self.specialisation = specialisation
-        
-    def __repr__(self):
-        return '<Product %d>' % self.id
-    
-    
-class AuthorSchema(ma.Schema):
-    class Meta:
-        model = Author
-        sqla_session = db.session
-        
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    specialisation = fields.String(required=True)
-    
-    def __repr__(self):
-        return '<Product %d>' % self.id
-    
-with app.app_context():    
-    db.create_all()
-    
-@app.before_request
-def before_request():
-    print(request.method, request.endpoint)
-    
-@app.route('/authors', methods = ['GET'])
-def index():
-    get_authors = Author.query.all()
-    author_schema = AuthorSchema(many=True, only=['id', 'name', 'specialisation'])
-    authorr, error = author_schema.dump(get_authors)
-    return make_response(jsonify({"authors": authorr}))  
+openai.api_key = CHATGPT_ENV
+bot = Bot(TELEGRAM_BOT_API_ENV)
 
-@app.route('/authors', methods = ['POST'])
-def create_author():
-  data = request.get_json()
-  author_schema = AuthorSchema()
-  authorr, error = author_schema.load(data)
-  print(f'authors: {type(authorr)}, {authorr}, data {data}')
-  result = author_schema.dump(authorr.create()).data
-  return make_response(jsonify({"author": result}),200)  
+async def send_telegram_message(bot, chat_id, message):
+    await bot.send_message(chat_id=chat_id, text=message)
 
+@app.route('/question', methods=['POST'])
+async def question():
+    data = request.get_json()
+    question = data['question']
+    print(f'Question : {question}')
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=question,
+        max_tokens=256,
+        n=1
+    )
+    response_text = response['choices'][0]['text']
+    print(f'Response : {response_text}') 
+    tts = gTTS(response_text, lang='tr', tld="com")
+    tts.save('response.mp3')
+    audio_file = BytesIO()
+    tts.write_to_fp(audio_file)
+    asyncio.ensure_future(send_telegram_message(bot=bot,
+                                                chat_id='756050439',
+                                                message=response_text))
+    #bot.send_message(chat_id='756050439', text=response_text)
+    return response_text
 
 if __name__ == '__main__':
     app.run(debug=True)
