4c21cbfc2a863a45ab60a0294747e12c1dd969de
kozanakyel <kozanakyel@gmail.com>
2023-10-27 20:19:10 +0300

    added pthread_join and detach examples

--- threadsTry.c
+++ threadsTry.c
@@ -1,30 +1,48 @@
-#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdint.h> // Include this for intptr_t
 
-#define NUMBER_OF_THREADS   10
+#define NUMBER_OF_THREADS 10
 
-void *print_hello_world(void *tid){
-    printf("Hello World. Greetings from thread: %d\n",tid);
-    // pthread_attr_destroy();
+void *print_hello_world(void *tid) {
+    printf("Hello World. Greetings from thread: %ld\n", (intptr_t)tid);
     pthread_exit(NULL);
-
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     pthread_t threads[NUMBER_OF_THREADS];
     int status, i;
 
-    for (int i = 0; i < NUMBER_OF_THREADS; i++)
-    {
+    for (i = 0; i < NUMBER_OF_THREADS; i++) {
         printf("Main Here. Creating thread %d\n", i);
-        status = pthread_create(&threads[i], NULL, print_hello_world, (void *)i);
+        status = pthread_create(&threads[i], NULL, print_hello_world, (void *)(intptr_t)i);
 
-        if(status != 0){
-            printf("Oooops, pthread_create returned eror code %d\n", status);
+        if (status != 0) {
+            printf("Oops, pthread_create returned error code %d\n", status);
             exit(-1);
         }
     }
-    exit(NULL);
-    
+
+    // Wait for all threads to finish
+    for (i = 0; i < NUMBER_OF_THREADS; i++) {
+        status = pthread_join(threads[i], NULL);
+        if (status != 0) {
+            printf("Oops, pthread_join returned error code %d\n", status);
+            exit(-1);
+        }
+        printf("Main Here. Joined with thread %d\n", i);
+    }
+
+    // Clean up threads (optional)
+    for (i = 0; i < NUMBER_OF_THREADS; i++) {
+        status = pthread_detach(threads[i]);
+        if (status != 0) {
+            printf("Oops, pthread_detach returned error code %d\n", status);
+            exit(-1);
+        }
+    }
+
+    printf("Main Here. All threads joined and cleaned up.\n");
+    pthread_exit(NULL);
 }
