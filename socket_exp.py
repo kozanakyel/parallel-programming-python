0604e4aaa6276a49f3dea42489573a1321e787a0
kozanakyel <kozanakyel@gmail.com>
2023-11-20 21:31:15 +0300

    selector bASED i/o socket server

--- socket_exp.py
+++ socket_exp.py
@@ -6,21 +6,36 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_address = ('127.0.0.1', 8000)
 server_socket.bind(server_address)
 server_socket.listen()
+server_socket.setblocking(False)
+
+connections = []
 
 try:
-    connection, client_address = server_socket.accept()
-    print(f'I got a connection from {client_address}!')
+    while True:
+        try:
+            connection, client_address = server_socket.accept()
+            connection.setblocking(False)
+            print(f'I got a connection from {client_address}!')
+            connections.append(connection)
+        except BlockingIOError:
+            pass
 
-    buffer = b''
+        for connection in connections:
+            try:
+                buffer = b''
 
-    while buffer[-2:] != b'\r\n':
-        data = connection.recv(2)
-        if not data:
-            break
-        else:
-            print(f'I got data: {data}!')
-            buffer = buffer + data
+                while buffer[-2:] != b'\r\n':
+                    data = connection.recv(2)
+                    if not data:
+                        break
+                    else:
+                        print(f'I got data: {data}!')
+                        buffer = buffer + data
 
-    print(f"All the data is: {buffer}")
+                print(f"All the data is: {buffer}")
+                connection.send(buffer)
+            except BlockingIOError:
+                pass
+
 finally:
     server_socket.close()
